1b876246ff8b7f316215447a65d4e2cb3590adb1
Raymond Mulligan <115628490+Raymond-Mulligan@users.noreply.github.com>
2024-11-01 14:28:58 +0200

    End of Section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
 import { store, StoreContext } from './app/stores/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/routes.tsx'
